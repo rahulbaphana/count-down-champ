86676bdad1ea827553d5cbb9c01bef8be7d7d445
Rahul Baphana <rahul.kbaphana@yahoo.com>
2017-12-16 13:25:33 +0530

    Add styling through .css to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
     render() {
         return(
-            <div>This is Countdown App Component!</div>
+            <div className="App">
+                <div className="App-title">
+                    Countdown to December 25, 2017
+                </div>
+
+                <div>
+                    <div className="Countdown-days">10 days</div>
+                    <div className="Countdown-hours">30 hours</div>
+                    <div className="Countdown-minutes">15 minutes</div>
+                    <div className="Countdown-seconds">20 seconds</div>
+                </div>
+
+                <div>
+                    <input placeholder='new date'/>
+                    <button>Submit</button>
+                </div>
+            </div>
         )
     }
 }
