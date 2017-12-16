6b9df5b9f94047b9208f67c215d99efea08340ea
Rahul Baphana <rahul.kbaphana@yahoo.com>
2017-12-16 13:29:20 +0530

    Add [static] state to component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2017'
+        }
+    }
+
     render() {
         return(
             <div className="App">
                 <div className="App-title">
-                    Countdown to December 25, 2017
+                    Countdown to {this.state.deadline}
                 </div>
 
                 <div>
